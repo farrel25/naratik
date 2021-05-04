31412612bf28cf3b39c28cae6ee6083bc2096380
Suhaili Faruq <suhaili.faruq01@gmail.com>
2021-05-04 16:26:43 +0700

    Initial Commit

---
+++ app/src/main/java/com/b21cap0051/naratik/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.b21cap0051.naratik
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
