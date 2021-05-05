af4a150706dd52fb609e2d33efe1efe424542e2d
Firsta Adi Pradana <firstaadi@hotmail.com>
2021-05-05 16:52:45 +0700

    Add error handling for /motif endpoint

--- routes/motif.js
+++ routes/motif.js
@@ -22,22 +22,31 @@ router.get('/', async(req, res) => {
     const id = search_params.get('id');
     // Search for file with given id in collection
     const allData = []
-    try {
-        await db.collection('motif')
-            // Search document based on unique ID of the classification task
-            .doc(id)
-            // Get the value inside that document
-            .get()
-            // Store the value on allData[]
-            .then((value) => {
-                allData.push(value.data())
-            })
-        // Response with motif and unique ID
-        res.json(allData[0])
-    }catch (e) {
-        res.send("ID not found")
-    }
-        });
+        try {
+            await db.collection('motif')
+                // Search document based on unique ID of the classification task
+                .doc(id)
+                // Get the value inside that document
+                .get()
+                // Store the value on allData[]
+                .then((value) => {
+                    allData.push(value.data())
+                })
+            // Check if data fetched successfully
+            try{
+                // This will trigger error if data not fetched successfully
+                console.log(allData[0]["motif_1"])
+                // This will send response with valid data if data fetched successfully
+                res.json(allData[0])
+            }catch{
+                // Send response if data not fetched successfully
+                res.send("Please try again later")
+            }
+        } catch (e) {
+            // Will triggered if URL is invalid
+            res.send("Invalid URL")
+        }
+    });
 
 
 module.exports = router;
