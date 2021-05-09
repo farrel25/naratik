2152a0888643ef329458b79998372f7c3df17bcf
babhistta <babhistta@gmail.com>
2021-05-09 17:36:19 +0700

    Add Bottom Bar
    + Connectiong with Fragment
    + Add icon
    - Change icon from outlined to filled

--- app/src/main/java/com/b21cap0051/naratik/ui/home/HomeActivity.kt
+++ app/src/main/java/com/b21cap0051/naratik/ui/home/HomeActivity.kt
@@ -1,8 +1,15 @@
 package com.b21cap0051.naratik.ui.home
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.b21cap0051.naratik.R
 import com.b21cap0051.naratik.databinding.ActivityHomeBinding
+import com.b21cap0051.naratik.ui.home.homefragment.AccountFragment
+import com.b21cap0051.naratik.ui.home.homefragment.CameraFragment
+import com.b21cap0051.naratik.ui.home.homefragment.ExploreFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
 
 class HomeActivity : AppCompatActivity() {
     private lateinit var binding : ActivityHomeBinding
@@ -10,5 +17,29 @@ class HomeActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val bottomNav = findViewById<BottomNavigationView>(R.id.bottomBar)
+        bottomNav.setOnNavigationItemSelectedListener(navListener)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction().replace(
+                R.id.fragment_container,
+                ExploreFragment()
+            ).commit()
+        }
+
     }
+
+    private val navListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+            var selectedFragment: Fragment? = null
+            when (item.itemId) {
+                R.id.item_nav_account -> selectedFragment = AccountFragment()
+                R.id.item_nav_home -> selectedFragment = ExploreFragment()
+                R.id.item_nav_camera -> selectedFragment = CameraFragment()
+            }
+            supportFragmentManager.beginTransaction().replace(
+                R.id.fragment_container, selectedFragment!!
+            ).commit()
+            true
+        }
+
 }
