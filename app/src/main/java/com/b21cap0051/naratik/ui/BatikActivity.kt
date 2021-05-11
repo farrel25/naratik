a4455b91efce2d3ae7d00a5a96758c3d6cd265d5
Suhaili Faruq <suhaili.faruq01@gmail.com>
2021-05-11 15:03:39 +0700

    add and make custom action bar for all activity,button liked,and add some kotlin code for staggered layout in batik activity

--- app/src/main/java/com/b21cap0051/naratik/ui/BatikActivity.kt
+++ app/src/main/java/com/b21cap0051/naratik/ui/BatikActivity.kt
@@ -5,14 +5,25 @@ import android.graphics.drawable.ColorDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
 import com.b21cap0051.naratik.R
+import com.b21cap0051.naratik.adapter.BatikListAdapter
 import com.b21cap0051.naratik.databinding.ActivityBatikBinding
+import com.b21cap0051.naratik.model.BatikModel
+import com.b21cap0051.naratik.util.ItemBatikCallBack
 
-class BatikActivity : AppCompatActivity() {
+class BatikActivity : AppCompatActivity(),ItemBatikCallBack {
+    private lateinit var BatikAdapter : BatikListAdapter
     private lateinit var binding : ActivityBatikBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityBatikBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        BatikAdapter = BatikListAdapter(this)
+        binding.rvBatik.layoutManager = StaggeredGridLayoutManager(2,StaggeredGridLayoutManager.VERTICAL)
+        binding.rvBatik.adapter = BatikAdapter
+    }
+    override fun itemBatikClick(model: BatikModel) {
+
     }
 }
