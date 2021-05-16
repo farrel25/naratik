a967339647b25d7d40f5de03aacfdf1961c8c898
Suhaili Faruq <suhaili.faruq01@gmail.com>
2021-05-16 07:58:05 +0700

    add ROOM and retrofit dependencies,make class for Data resource

---
+++ app/src/main/java/com/b21cap0051/naratik/dataresource/api/APIConfig.kt
@@ -0,0 +1,31 @@
+package com.b21cap0051.naratik.dataresource.api
+
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class APIConfig
+{
+	companion object
+	{
+		fun getBatikService():ApiService
+		{
+			val interceptor = HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
+			val Client = OkHttpClient.Builder()
+				.addInterceptor(interceptor)
+				.build()
+			
+			val API = Retrofit.Builder()
+				.baseUrl("")
+				.addConverterFactory(GsonConverterFactory.create())
+				.client(Client)
+				.build()
+			
+			return API.create(ApiService::class.java)
+		}
+		
+	}
+	
+	
+}
