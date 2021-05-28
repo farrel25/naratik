8592697dc60b295f82250920519f2688a6138325
babhistta <babhistta@gmail.com>
2021-05-29 00:16:00 +0700

    Change conflict
    Add library shimmer

--- app/src/main/java/com/b21cap0051/naratik/ui/cameraui/UploadProcessFragment.kt
+++ app/src/main/java/com/b21cap0051/naratik/ui/cameraui/UploadProcessFragment.kt
@@ -1,14 +1,6 @@
 package com.b21cap0051.naratik.ui.cameraui
 
 import android.os.Bundle
-<<<<<<< Updated upstream
-=======
-<<<<<<< HEAD
-import android.os.Handler
-=======
->>>>>>> Stashed changes
-import androidx.fragment.app.Fragment
->>>>>>> 4c4913545203a416843919a836bb8dde9bdfcefe
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -17,18 +9,10 @@ import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.b21cap0051.naratik.databinding.FragmentUploadProcessBinding
-import com.b21cap0051.naratik.util.voapi.StatusResponse
 import com.b21cap0051.naratik.dataresource.remotedata.model.ImageUploadModel
 import com.b21cap0051.naratik.mainview.UploadMainView
 import com.b21cap0051.naratik.mainview.ViewFactoryModel
-<<<<<<< Updated upstream
 import com.b21cap0051.naratik.util.vo.Status
-=======
-<<<<<<< HEAD
-=======
-import com.b21cap0051.naratik.util.vo.Status
->>>>>>> 4c4913545203a416843919a836bb8dde9bdfcefe
->>>>>>> Stashed changes
 
 
 class UploadProcessFragment : Fragment()
@@ -58,72 +42,38 @@ class UploadProcessFragment : Fragment()
 		
 		mainView.uploadFile(modelData)
 		
-<<<<<<< HEAD
 		mainView.GetProgress().observe(viewLifecycleOwner , { response ->
 			when (response.Status)
 			{
-				StatusResponse.EMPTY   ->
+				Status.LOADING   ->
 				{
 					binding.tvProgress.text = "${response.Data} %"
 					pb.progress = response.Data!!.toInt()
 				}
-				StatusResponse.SUCCESS ->
+				Status.SUCCESS ->
 				{
 					Toast.makeText(context , response.message , Toast.LENGTH_SHORT).show()
 				}
-				StatusResponse.ERROR   ->
+				Status.ERROR   ->
 				{
 					Toast.makeText(context , response.message , Toast.LENGTH_SHORT).show()
-=======
-		mainView.GetProgress().observe(viewLifecycleOwner,{ response ->
-			when(response.Status){
-				Status.LOADING    -> {
-					pb.max = 100
-					val currentProgress = response.Data?.toInt()
-					if (currentProgress != null)
-					{
-						ObjectAnimator.ofInt(binding.pbCameraUpload,"progress",currentProgress)
-					}
-//					pb.progress = "${response.Data} %"
-				}
-				Status.SUCCESS    -> {
-					Toast.makeText(context,response.message,Toast.LENGTH_SHORT).show()
-				}
-				Status.ERROR -> {
-					Toast.makeText(context,response.message,Toast.LENGTH_SHORT).show()
->>>>>>> 4c4913545203a416843919a836bb8dde9bdfcefe
 				}
 			}
 		})
 		
-<<<<<<< HEAD
 		mainView.IsDone().observe(viewLifecycleOwner , { response ->
 			when (response.Status)
 			{
-				StatusResponse.EMPTY   ->
+				Status.LOADING ->
 				{
 					showText(response.Data!!)
 				}
-				StatusResponse.SUCCESS ->
+				Status.SUCCESS  ->
 				{
 					showText(response.Data!!)
 				}
-				StatusResponse.ERROR   ->
+				Status.ERROR    ->
 				{
-=======
-		mainView.IsDone().observe(viewLifecycleOwner,{response ->
-			when(response.Status){
-				Status.LOADING    -> {
-					showText(response.Data!!)
-				}
-				Status.SUCCESS    -> {
-					showText(response.Data!!)
-				}
-				Status.ERROR -> {
-<<<<<<< Updated upstream
-=======
->>>>>>> 4c4913545203a416843919a836bb8dde9bdfcefe
->>>>>>> Stashed changes
 					showText(response.Data!!)
 				}
 			}
