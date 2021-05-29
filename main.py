069ea28ab9dc501cd6474c273ae44911dada4bd9
Firsta Adi Pradana <firstaadi@hotmail.com>
2021-05-29 21:56:07 +0700

    Initial Commit

---
+++ main.py
@@ -0,0 +1,14 @@
+import requests
+def predict_motif(event, context):
+    """Triggered by a change to a Cloud Storage bucket.
+    Args:
+         event (dict): Event payload.
+         context (google.cloud.functions.Context): Metadata for the event.
+    """
+    file = event
+    print(f"Processing file: {file['name']}.")
+    BASE_URL = "<prediction endpoint>"
+    FILE_NAME = file["name"]
+    FINAL_URL = BASE_URL + FILE_NAME
+    requests.get(FINAL_URL)
+
